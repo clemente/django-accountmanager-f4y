5882b36b0210e4dc6139a0fac3616a384bec2d0d
dc <dc@noad>
2017-06-01 10:36:56 +0200

    added Account class and admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from accounts.models import Account
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('number', 'currency', 'creation_date')
+
+admin.site.register(Account, AccountAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+ALLOWED_CURRENCIES = [
+    ("USD", "USD: US dollar"),
+    ("EUR", "EUR: Euro"),
+    ("GBP", "GBP: British pound"),
+    ("CHF", "CHF: Swiss frank"),
+]
+
+def validate_8digits(number):
+    if not (10000000 <= number <= 99999999):
+        raise ValidationError("Account numbers must be 8 digits. Yours has %i"%len(str(number)))
+
+class Account(models.Model):
+    number = models.PositiveIntegerField(help_text="Account number, 8 digits",unique=True,validators=[validate_8digits])
+    currency = models.CharField(max_length=3,choices=ALLOWED_CURRENCIES)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "Account number %i"%self.number
+
+#class Transaction(models.Model):
+    

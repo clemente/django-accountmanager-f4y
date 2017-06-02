01a35f295ca06d4b7d4efdf83b28c80c3c6577b7
dc <dc@noad>
2017-06-02 02:27:55 +0200

    adapted the JSON response in case of error, both in grammar error as in semantic error, and create a dictionary of short error codes to include in the response

--- accounts/api.py
+++ accounts/api.py
@@ -6,6 +6,14 @@ from accounts.models import Account, Transaction
 import datetime
 
 
+ERROR_CODES = {
+    'm_par': 'Missing parameters',
+    'm_destacc': "Missing destination account",
+    'm_srcacc': "Missing source account",
+    'm_am': "Missing amount",
+    'nf_acc': "Account doesn't exist",
+}
+
 # e.g. http://127.0.0.1:8000/api/account/?format=json
 # See scripts/api_client_calls.sh to test this
 class AccountResource(ModelResource):
@@ -18,7 +26,7 @@ class AccountResource(ModelResource):
 
     def dehydrate(self, bundle):
         """Create the appropriate response, e.g. include an "error" attribute in the response"""
-        # TODO add nice error handling (and set error=True)
+        # TODO add nice error handling (and set error=True). E.g. invalid currency
         orig_data=dict(bundle.data)
         bundle.data={'error':False, 'data':orig_data}
         return bundle
@@ -26,6 +34,8 @@ class AccountResource(ModelResource):
 
 class TransactionInputValidation(Validation):
     def is_valid(self, bundle, request=None):
+        """This checks basic formats, e.g. the parameters should be present.
+        Similar validations are done later in obj_create but they're semantic (e.g. the given accounts must exist)"""
         if not bundle.data:
             return {'__all__': 'Missing parameters'}
 
@@ -38,12 +48,7 @@ class TransactionInputValidation(Validation):
         if 'amount' not in bundle.data:
             errors['amount']="Missing amount"
 
-        # for key, value in bundle.data.items():
-        #     if not isinstance(value, basestring):
-        #         continue
-        # 
-        #     if not 'awesome' in value:
-        #         errors[key] = ['NOT ENOUGH AWESOME. NEEDS MORE.']
+        #print("Returning errors:",errors)
 
         return errors
 
@@ -62,60 +67,99 @@ class TransactionResource(ModelResource):
         """Extend input data to make it match with model data."""
         #bundle = super(TransactionResource, self).obj_create(bundle, request, user=request.user)
 
-        # Validator already checked this:
-        assert 'sourceAccount' in bundle.data
-        assert 'destAccount' in bundle.data
-        assert 'amount' in bundle.data
+        # Only try to find accounts etc. if the parameters are valid (they exist, valid format, …)
+        if self.is_valid(bundle):
 
-        try:
-            dest_acc=Account.objects.get(number=bundle.data['destAccount'])
-        except Account.DoesNotExist:
-            dest_acc=None
-        try:
-            source_acc=Account.objects.get(number=bundle.data['sourceAccount'])
-        except Account.DoesNotExist:
-            source_acc=None
-
-        # TODO act on errors
-
-
-        if bundle.data['sourceAccount'] is None:
-            bundle.obj.op_type='dep'
-            # bundle.data['dest_acc']=bundle.data['destAccount']
-            bundle.obj.dest_acc=dest_acc
-            bundle.obj.dest_amount=bundle.data['amount']
-            #bundle.data['dest_amount']=bundle.data['amount']
-            bundle.obj.source_acc=None
-            bundle.obj.source_amount=None
-        elif bundle.data['destAccount'] is None:
-            bundle.obj.op_type='wd'
-            bundle.obj.source_acc=source_acc
-            bundle.obj.source_amount=bundle.data['amount']
-            bundle.obj.dest_acc=None
-            bundle.obj.dest_amount=None
-        elif bundle.data['sourceAccount'] and bundle.data['destAccount']:
-            bundle.obj.op_type='tra'
-            bundle.obj.source_acc=source_acc
-            bundle.obj.dest_acc=dest_acc
-            # FIXME detect intercurrency transfers and store different amounts
-            bundle.obj.source_amount=bundle.data['amount']
-            bundle.obj.dest_amount=bundle.data['amount']
+            # Validator will already checked this:
+            assert 'sourceAccount' in bundle.data
+            assert 'destAccount' in bundle.data
+            assert 'amount' in bundle.data
+    
+            # This is what tastypie calls "hydrate": transform text→model. It may get None if we don't find the IDs
+            try:
+                dest_acc=Account.objects.get(number=bundle.data['destAccount'])
+            except Account.DoesNotExist:
+                dest_acc=None
+            try:
+                source_acc=Account.objects.get(number=bundle.data['sourceAccount'])
+            except Account.DoesNotExist:
+                source_acc=None
+    
+            # Depending on transaction type, fill some data or other. Also, require different data
+            if bundle.data['sourceAccount'] is None:
+                bundle.obj.op_type='dep'
+                # bundle.data['dest_acc']=bundle.data['destAccount']
+                if not dest_acc:
+                    bundle.errors['destAccount']="Account doesn't exist"
+                bundle.obj.dest_acc=dest_acc
+                bundle.obj.dest_amount=bundle.data['amount']
+                #bundle.data['dest_amount']=bundle.data['amount']
+                bundle.obj.source_acc=None
+                bundle.obj.source_amount=None
+    
+            elif bundle.data['destAccount'] is None:
+                bundle.obj.op_type='wd'
+                if not source_acc:
+                    bundle.errors['sourceAccount']="Account doesn't exist"
+                bundle.obj.source_acc=source_acc
+                bundle.obj.source_amount=bundle.data['amount']
+                bundle.obj.dest_acc=None
+                bundle.obj.dest_amount=None
+    
+            elif bundle.data['sourceAccount'] and bundle.data['destAccount']:
+                bundle.obj.op_type='tra'
+                if not dest_acc:
+                    bundle.errors['destAccount']="Account doesn't exist"
+                if not source_acc:
+                    bundle.errors['sourceAccount']="Account doesn't exist"
+                bundle.obj.source_acc=source_acc
+                bundle.obj.dest_acc=dest_acc
+                # FIXME detect intercurrency transfers and store different amounts
+                bundle.obj.source_amount=bundle.data['amount']
+                bundle.obj.dest_amount=bundle.data['amount']
+    
+            else:
+                raise Exception("check validator")
+    
+            # extra data
+            bundle.obj.date = datetime.datetime.utcnow()
+    
+            # No need to do hydrate (== transforming key/value to objects) because we did it above
+            #bundle = self.full_hydrate(bundle)
         else:
-            raise Exception("check validator")
-
-        # extra data
-        bundle.obj.date = datetime.datetime.utcnow()
-
-        # No need to do hydrate (== transforming key/value to objects) because we did it above
-        #bundle = self.full_hydrate(bundle)
+            # if not valid, the save() will not save it
+            pass
 
         return self.save(bundle)
 
     def dehydrate(self, bundle):
         """Create the appropriate response, e.g. include an "error" attribute in the response"""
-        # TODO add nice error handling (and set error=True)
+        # TODO add more error handling (and set error=True). E.g. invalid amount
+        bundle.data['transactionId']=bundle.data.pop('id') # rename key
         orig_data=dict(bundle.data)
         bundle.data={'error':False, 'data':orig_data}
         return bundle
 
+    def error_response(self, request, errors, response_class=None):
+        """Wrap the original error handler in order to change the message format according to the requirements"""
+        # Normalize errors. The ones from validator are "transactions: {…}", the ones from obj_create are "{…}"
+        if 'transactions' in errors:
+            errors=errors['transactions']
 
+        #import ipdb; ipdb.set_trace()
+
+        # the "errors" dict could have many errors but our response format mandates to tell only one. So we take any of them
+        error_msg=next(iter(errors.values())) # details: https://stackoverflow.com/questions/3097866/access-an-arbitrary-element-in-a-dictionary-in-python
+        codes_for_msg=[k for k,v in ERROR_CODES.items() if v==error_msg] # find key for the given value. It's a dictionary access in reverse. 
+        if len(codes_for_msg)!=1:
+            raise NotImplementedError("Error message '%s' has no short code defined. Check api.py. %s"%(error_msg,codes_for_msg))
+        
+        error_code=codes_for_msg[0]
+
+        # rewrite the error dict
+        errors={"error": True, "code": error_code, "message": error_msg}
+
+        # continue normally but with our dict
+        res=super(TransactionResource,self).error_response(request, errors, response_class)
+
+        return res
